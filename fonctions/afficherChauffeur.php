2730d945ecc29850ab87231e66a99905e31ec6f7
raphael <User@Raphael.tgi.sn>
2024-07-03 19:14:45 +0000

    INFORMATION

--- fonctions/afficherChauffeur.php
+++ fonctions/afficherChauffeur.php
@@ -13,24 +13,24 @@ function afficherCauffeur() {
       $query = "SELECT * FROM chauffeur"; 
       $stmt = $conn->query($query);
 
-      while ($rowStatistique = $stmt->fetch(PDO::FETCH_NUM)) {
+      while ($rowInformation = $stmt->fetch(PDO::FETCH_NUM)) {
           ?>
           <div>
               <table>
                   <tr>
-                      <td> <?php echo $rowStatistique[0]  ?> </td>
-                      <td> <?php echo $rowStatistique[1]   ?> </td>
-                      <td> <?php echo $rowStatistique[2]   ?> </td>
-                      <td> <?php echo $rowStatistique[3]   ?> </td>
-                      <td> <?php echo $rowStatistique[4]   ?> </td>
-                      <td> <?php echo $rowStatistique[5]   ?> </td>
-                      <td> <?php echo $rowStatistique[6]   ?> </td>
-                      <td> <?php echo $rowStatistique[7]   ?> </td>
-                      <td> <?php echo $rowStatistique[8]   ?> </td>
-                      <td> <?php echo $rowStatistique[9]   ?> </td>
+                      <td> <?php echo $rowInformation[0]  ?> </td>
+                      <td> <?php echo $rowInformation[1]   ?> </td>
+                      <td> <?php echo $rowInformation[2]   ?> </td>
+                      <td> <?php echo $rowInformation[3]   ?> </td>
+                      <td> <?php echo $rowInformation[4]   ?> </td>
+                      <td> <?php echo $rowInformation[5]   ?> </td>
+                      <td> <?php echo $rowInformation[6]   ?> </td>
+                      <td> <?php echo $rowInformation[7]   ?> </td>
+                      <td> <?php echo $rowInformation[8]   ?> </td>
+                      <td> <?php echo $rowInformation[9]   ?> </td>
                       <td style="background-color: #0099ff00;">
                           <form action="gestiondesdonnees.php">
-                              <input type="text" name="permis" value="<?php echo $rowStatistique[0] ?>"  style="position: absolute;width: 50px;height: 20px;opacity: 0;top: 5px;">
+                              <input type="text" name="permis" value="<?php echo $rowInformation[0] ?>"  style="position: absolute;width: 50px;height: 20px;opacity: 0;top: 5px;">
                               <input type="submit" class="voir-detail" value="Supprimer" style="position: relative;width: 102px;height: 30px;border: 0;border-radius: 5px;" name="suppression">
                           </form>
                       </td>
