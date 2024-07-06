8a92a87c2c2996766377714e09ffd0ac70110242
raphael <User@Raphael.tgi.sn>
2024-07-06 18:59:50 +0000

    bouton

--- src/bilanindividuel.php
+++ src/bilanindividuel.php
@@ -9,48 +9,61 @@ require '../fonctions/message.php';
     <title>DK-7852-AS</title>
     <link rel="stylesheet" href="../style/bilanindividuel.css">
     <style>
-      .kilometrage{
-    width: 30%;
-    position: absolute;
-    top: 68%;
-    left: 5%;
-  }
-  .voir-detail{
-    /* display: block; */
-    /* width: 100%; */
-    font-weight: bold;
-    background: linear-gradient(45deg, rgb(16, 137, 211) 0%, rgb(18, 177, 209) 100%);
-    color: white;
-    /* padding-block: 15px; */
-    /* margin: 20px auto; */
-    /* border-radius: 20px; */
-    /* box-shadow: rgba(133, 189, 215, 0.8784313725) 0px 20px 10px -15px; */
-    border: none;
-    transition: all 0.2s ease-in-out;
-  }
-  .voir-detail:hover {
-    transform: scale(1.03);
-    background: linear-gradient(45deg, rgb(0, 108, 175) 0%, rgb(16, 137, 211) 100%);
-    box-shadow: rgba(133, 189, 215, 0.8784313725) 0px 23px 10px -20px;
-  }
-  td{
-    /* min-width: 90px; */
-  }
+     a{
+        color: rgb(0, 0, 0);
+        text-decoration: none;
+      }
+.button {
+ --glow-color: rgb(176, 217, 255);
+ --enhanced-glow-color: rgb(206, 235, 255);
+ --btn-color: rgb(255, 255, 255);
+ border: .20em solid var(--glow-color);
+ padding: 0.5em 1.5em;
+ color: var(--glow-color);
+ font-size: 15px;
+ font-weight: bold;
+ background-color: var(--btn-color);
+ border-radius: 1em;
+ outline: none;
+ box-shadow: 0 0 1em .5em var(--glow-color),
+        0 0 4em 1em var(--glow-spread-color),
+        inset 0 0 .5em .25em var(--glow-color);
+ text-shadow: 0 0 .5em var(--glow-color);
+ position: fixed;
+ top:10px;
+ transition: all 0.3s;
+}
+
+button::after {
+ pointer-events: none;
+ content: "";
+ position: absolute;
+ top: 120%;
+ left: 0;
+ height: 100%;
+ width: 100%;
+ background-color: var(--glow-spread-color);
+ filter: blur(2em);
+ opacity: .7;
+ transform: perspective(1.5em) rotateX(35deg) scale(1, .6);
+}
+
+button:hover {
+ color: var(--btn-color);
+ background-color: var(--glow-color);
+ box-shadow: 0 0 1em .5em var(--glow-color),
+        0 0 4em 2em var(--glow-spread-color),
+        inset 0 0 .5em .5em var(--glow-color);
+}
+
+button:active {
+ box-shadow: 0 0 0.6em .25em var(--glow-color),
+        0 0 2.5em 2em var(--glow-spread-color),
+        inset 0 0 .5em .25em var(--glow-color);
+}
     </style>
 </head>
 <body>
-  
-    <div class="container">
-        <form action="services.php" methode="post" class="container">
-
-        <?php require '../fonctions/afficherInfosVehicule.php';
-        
-        ?>
-
-        </form>
-        
-    </div>
-
     
     <div class="historique">
       <div class="heading">historique</div>
@@ -92,7 +105,11 @@ require '../fonctions/message.php';
       </section>
     </div>
 
-    
+    <button class="button">
+        <a href="bilangeneral.php">
+          Retour
+        </a>
+      </button>
 
 </body>
 
